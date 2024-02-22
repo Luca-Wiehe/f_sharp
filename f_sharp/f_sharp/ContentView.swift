8589106f52fb4bc77bce39a88309ea49823daef5
Luca-Wiehe <luca@wiehe.com>
2024-02-22 11:03:27 +0100

    Implemented Draft for SettingsView

--- f_sharp/f_sharp/ContentView.swift
+++ f_sharp/f_sharp/ContentView.swift
@@ -6,7 +6,8 @@
 //
 
 import SwiftUI
-import JavaScriptCore
+import SwiftSVG
+
 
 struct ContentView: View {
     var body: some View {
@@ -31,11 +32,64 @@ struct ContentView: View {
                     Label("Search", systemImage: "magnifyingglass")
                 }
 
-            Text("Settings")
+            SettingsView()
                 .tabItem {
                     Label("Settings", systemImage: "gear")
                 }
         }
+    }
+}
+
+struct SettingsView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var darkModeIsOn: Bool = true
+    @State private var soundSettingsIsOn: Bool = true
+    @State private var privacySettingsIsOn: Bool = false
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Settings")
+                .font(.largeTitle)
+                .bold()
+                .padding(.leading, 24)
+                .padding(.top, 12)
+
+            Form {
+                // General Settings
+                Section(header: Text("General")) {
+                    Toggle("Dark Mode", isOn: $darkModeIsOn)
+                    Picker("First Day of the Week", selection: .constant(1)) {
+                        Text("Sunday").tag(1)
+                        Text("Monday").tag(2)
+                        // Add more days if needed
+                    }
+                    Toggle("Sound Settings", isOn: $soundSettingsIsOn)
+                }
+                
+                // Account Settings
+                Section(header: Text("Account")) {
+                    TextField("Username", text: $username)
+                    TextField("E-Mail", text: $email)
+                    SecureField("Password", text: $password)
+                }
+                
+                // Privacy Settings
+                Section(header: Text("Privacy")) {
+                    Text("Terms of Use")
+                    Toggle("Privacy Settings", isOn: $privacySettingsIsOn)
+                }
+                
+                // Support Settings
+                Section(header: Text("Support")) {
+                    Button("Contact Us") {
+                    }
+                }
+            }
+            Spacer()
+        }
+        .padding(.top, 1)
     }
 }
 
